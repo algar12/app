9d425e06e53a8aeb39bf00571df183d0e39ee46f
algar12 <algar0940@gmail.com>
2025-03-06 04:51:42 +0700

    r2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,9 @@
 
     <title> {{ isset($title) ? $title . ' - ' : '' }}{{ config('app.name', '') }}</title>
 
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-    <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-10">
+    <link rel="icon" sizes="144x144" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
