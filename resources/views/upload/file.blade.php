54bdf22a4e92c51d9b1331dea01beff224c72e4b
algar12 <algar0940@gmail.com>
2025-03-06 01:33:54 +0700

    ubah

--- resources/views/upload/file.blade.php
+++
@@ -1,7 +0,0 @@
-<x-app-layout :title="'Upload File'">
-    <form action="{{ route('upload.file') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <input type="file" name="file" />
-        <button type="submit">Upload</button>
-    </form>
-</x-app-layout>
