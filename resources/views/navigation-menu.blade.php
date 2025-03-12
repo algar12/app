28c79e10cb4400156531a5964817110956f21807
algar12 <algar0940@gmail.com>
2025-03-12 17:25:02 +0700

    logo

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -3,7 +3,7 @@
         <!-- Left Section -->
         <div class="flex items-center space-x-4">
         <a href="{{ route('home') }}" class="flex items-center text-xl font-semibold bg-gradient-to-r from-green-500 to-yellow-500 text-transparent bg-clip-text">
-    <x-application-mark />
+    <!-- <x-application-mark /> -->
     <span class="ml-2">IPNU IPPNU TANJUNGHARJO</span>
 </a>
             
